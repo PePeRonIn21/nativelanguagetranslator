f905b5cafd38f30e8bb30ce47cf5db07011702a8
PePe_Ronin <davedccalapis@gmail.com>
2024-01-09 18:28:27 +0800

    with firebase setuo

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'loading_page.dart';
 
-void main() => runApp(TranslatorApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+}
 
 class TranslatorApp extends StatelessWidget {
   @override
